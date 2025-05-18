b285adfdd07ea6051256755e88dd2de33b88046f
Naveed Hayat <163723443+Zennido@users.noreply.github.com>
2025-05-18 17:45:38 +0500

    Update about.py

--- pages/about.py
+++ pages/about.py
@@ -54,7 +54,7 @@ with col1:
     """)
 
 with col2:
-    st.image("https://ibb.co/wr26MdJy", use_column_width=True)
+    st.image("https://www.imghippo.com/i/wOPs3437doc.png", use_column_width=True)
     
     st.markdown("""
     ## NidoData
